4e884dce82672a3e2a89f52bdb52672378977003
Ali & Shlok <samin@apigee.com>
2016-07-22 16:53:18 -0700

    All writes now update cluster. * The writes happen through a mutex lock. The lock is released only after the cluster is updated. * Either the transaction commits or rollbacks. The only scenario where inconsistent data state might be reached is when error happenes during rollback from a failed transaction.

--- services/clusterflux/client.go
+++ services/clusterflux/client.go
@@ -7,19 +7,17 @@ import (
 	"io/ioutil"
 	"math"
 	"net/http"
-	"strconv"
-	"strings"
+	"net/url"
+	"sync"
 	"time"
 
-	"github.com/gogo/protobuf/proto"
 	"github.com/influxdata/influxdb/influxql"
 	"github.com/influxdata/influxdb/services/meta"
 	"github.com/spf13/viper"
 )
 
-// VERSION tracks current version of the cluster
-var VERSION int
-var FAIL_ATTEMPTS int
+// Version tracks current version of the cluster
+var Version string
 
 // Client is used as a wrapper on meta client to execute
 // commands on and read data from meta service cluster.
@@ -27,12 +25,17 @@ type Client struct {
 	*meta.Client
 }
 
+type ClusterResponse struct {
+	Body    []byte
+	Version string
+	Status  int
+}
+
 // NewClient returns a new *Client.
 func NewClient(config *meta.Config) *Client {
 	viper.SetDefault("CFLUX_ENDPOINT", "localhost:8000")
 	viper.SetDefault("CLUSTER", "default")
-	VERSION = 0
-	FAIL_ATTEMPTS = 0
+	Version = "0"
 	return &Client{
 		meta.NewClient(config),
 	}
@@ -70,108 +73,38 @@ func (c *Client) Databases() []meta.DatabaseInfo {
 
 // CreateDatabase creates a database or returns it if it already exists
 func (c *Client) CreateDatabase(name string) (*meta.DatabaseInfo, error) {
-	db, err := c.Client.CreateDatabase(name)
-	if err != nil {
-		return db, err
+	f := func(c *Client) (interface{}, error) {
+		return c.Client.CreateDatabase(name)
 	}
-	_, err = c.postToCflux(viper.GetString("CLUSTER"), name)
-	if err != nil {
-		if strings.Contains(err.Error(), "Retry") {
-			c.CreateDatabase(name)
-		}
-		// should I delete the created database or retry? DB could be inconsistent
-		c.Client.DropDatabase(name)
-	}
-	return db, err
-}
-
-// pass in function. copy Data, do operation, revert to snapshot on failure
-// use defer
-// goto err handler
-func (c *Client) postToCflux(cluster string, dbName string) (ClusterResponse, error) {
-	var req *http.Response
-	cdata := c.Data()
-	response := &ClusterResponse{}
-	// if err return err
-	// revert to old snapshot and return err
-	data, err := (&cdata).MarshalBinary()
-	if err != nil {
-		if FAIL_ATTEMPTS < 5 {
-			FAIL_ATTEMPTS = FAIL_ATTEMPTS + 1
-			c.postToCflux(cluster, dbName)
-		}
-		FAIL_ATTEMPTS = 0
-		// Should/Shouldn't we drop the local copy if failed to put in cluster.
-		// move it to the caller (independent of who is calling)
-		c.Client.DropDatabase(dbName)
-		return *response, err
-	}
-	url := viper.GetString("CFLUX_ENDPOINT") + "/" + cluster + "/" + strconv.Itoa(VERSION)
-	// think fn programming
-	for attempt := 1; attempt < 6; attempt++ {
-		req, err = http.Post(url, "", bytes.NewBuffer(data))
-		if err == nil {
-			break
-		}
-		backoff := (math.Pow(2, float64(attempt)) - 1) / 2
-		time.Sleep(time.Duration(backoff) * time.Second)
-	}
-	if err != nil {
-		// Should/Shouldn't we drop the local copy if failed to put in cluster.
-		c.Client.DropDatabase(dbName)
-		return *response, errors.New("Error in posting to clusterflux. " + err.Error())
-	}
-	resp, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		// revert to snapshot and err out
-		c.Client.DropDatabase(dbName)
-		return *response, errors.New("ioutil.ReadAll failed: " + err.Error())
-		// retry?
-	}
-	err = proto.Unmarshal(resp, response)
-	if err != nil {
-		c.Client.DropDatabase(dbName)
-		return *response, errors.New("Failed unmarshaling response from cluster: " + err.Error())
-		// return *response, errors.New("Retry - prooto.unmarshal failed: " + err.Error())
-	}
-	ver, err := strconv.Atoi(response.GetVersion())
-	if err != nil {
-		// revert to snapshot and err out
-		c.Client.DropDatabase(dbName)
-		return *response, errors.New("Failed while converting string to integer: " + err.Error())
-	}
-	if !*response.Success {
-		if ver > VERSION {
-			err = (&cdata).UnmarshalBinary([]byte(response.GetData()))
-			if err != nil {
-				c.Client.DropDatabase(dbName)
-				return *response, errors.New("Failed unmarshaling response Data from cluster: " + err.Error())
-			}
-			return *response, errors.New("Retry on the updated Data")
-		}
-		c.Client.DropDatabase(dbName)
-		return *response, errors.New(response.GetMessage())
-	}
-	VERSION = ver
-	return *response, nil
+	val, err := c.ModifyAndSync(f)
+	return val.(*meta.DatabaseInfo), err
 }
 
 // CreateDatabaseWithRetentionPolicy creates a database with the specified retention policy.
-// TODO: protobuf
 func (c *Client) CreateDatabaseWithRetentionPolicy(name string, rpi *meta.RetentionPolicyInfo) (*meta.DatabaseInfo, error) {
-	return c.Client.CreateDatabaseWithRetentionPolicy(name, rpi)
+	f := func(c *Client) (interface{}, error) {
+		return c.Client.CreateDatabaseWithRetentionPolicy(name, rpi)
+	}
+	val, err := c.ModifyAndSync(f)
+	return val.(*meta.DatabaseInfo), err
 }
 
 // DropDatabase deletes a database.
-// TODO: protobuf
 func (c *Client) DropDatabase(name string) error {
-	return c.Client.DropDatabase(name)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.DropDatabase(name)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // CreateRetentionPolicy creates a retention policy on the specified database.
-// TODO: protobuf
 func (c *Client) CreateRetentionPolicy(database string, rpi *meta.RetentionPolicyInfo) (*meta.RetentionPolicyInfo, error) {
-	return c.Client.CreateRetentionPolicy(database, rpi)
+	f := func(c *Client) (interface{}, error) {
+		return c.Client.CreateRetentionPolicy(database, rpi)
+	}
+	val, err := c.ModifyAndSync(f)
+	return val.(*meta.RetentionPolicyInfo), err
 }
 
 // RetentionPolicy returns the requested retention policy info.
@@ -180,21 +113,30 @@ func (c *Client) RetentionPolicy(database, name string) (rpi *meta.RetentionPoli
 }
 
 // DropRetentionPolicy drops a retention policy from a database.
-// TODO: protobuf
 func (c *Client) DropRetentionPolicy(database, name string) error {
-	return c.Client.DropRetentionPolicy(database, name)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.DropRetentionPolicy(database, name)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // SetDefaultRetentionPolicy sets a database's default retention policy.
-// TODO: protobuf
 func (c *Client) SetDefaultRetentionPolicy(database, name string) error {
-	return c.Client.SetDefaultRetentionPolicy(database, name)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.SetDefaultRetentionPolicy(database, name)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // UpdateRetentionPolicy updates a retention policy.
-// TODO: protobuf
 func (c *Client) UpdateRetentionPolicy(database, name string, rpu *meta.RetentionPolicyUpdate) error {
-	return c.Client.UpdateRetentionPolicy(database, name, rpu)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.UpdateRetentionPolicy(database, name, rpu)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // Users foo
@@ -208,38 +150,53 @@ func (c *Client) User(name string) (*meta.UserInfo, error) {
 }
 
 // CreateUser foo
-// TODO: protobuf
 func (c *Client) CreateUser(name, password string, admin bool) (*meta.UserInfo, error) {
-	return c.Client.CreateUser(name, password, admin)
+	f := func(c *Client) (interface{}, error) {
+		return c.Client.CreateUser(name, password, admin)
+	}
+	val, err := c.ModifyAndSync(f)
+	return val.(*meta.UserInfo), err
 }
 
 // UpdateUser foo
-// TODO: protobuf
 func (c *Client) UpdateUser(name, password string) error {
-	return c.Client.UpdateUser(name, password)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.UpdateUser(name, password)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // DropUser foo
-// TODO: protobuf
 func (c *Client) DropUser(name string) error {
-	return c.Client.DropUser(name)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.DropUser(name)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // SetPrivilege foo
-// TODO: protobuf
 func (c *Client) SetPrivilege(username, database string, p influxql.Privilege) error {
-	return c.Client.SetPrivilege(username, database, p)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.SetPrivilege(username, database, p)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // SetAdminPrivilege foo
-// TODO: protobuf
 func (c *Client) SetAdminPrivilege(username string, admin bool) error {
-	return c.Client.SetAdminPrivilege(username, admin)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.SetAdminPrivilege(username, admin)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // UserPrivileges foo
 func (c *Client) UserPrivileges(username string) (map[string]influxql.Privilege, error) {
-	return c.UserPrivileges(username)
+	return c.Client.UserPrivileges(username)
 }
 
 // UserPrivilege foo
@@ -279,30 +236,42 @@ func (c *Client) ShardsByTimeRange(sources influxql.Sources, tmin, tmax time.Tim
 }
 
 // DropShard deletes a shard by ID.
-// TODO: protobuf
 func (c *Client) DropShard(id uint64) error {
-	return c.Client.DropShard(id)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.DropShard(id)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // CreateShardGroup creates a shard group on a database and policy for a given timestamp.
-// TODO: protobuf
 func (c *Client) CreateShardGroup(database, policy string, timestamp time.Time) (*meta.ShardGroupInfo, error) {
-	return c.Client.CreateShardGroup(database, policy, timestamp)
+	f := func(c *Client) (interface{}, error) {
+		return c.Client.CreateShardGroup(database, policy, timestamp)
+	}
+	val, err := c.ModifyAndSync(f)
+	return val.(*meta.ShardGroupInfo), err
 }
 
 // DeleteShardGroup removes a shard group from a database and retention policy by id.
-// TODO: protobuf
 func (c *Client) DeleteShardGroup(database, policy string, id uint64) error {
-	return c.Client.DeleteShardGroup(database, policy, id)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.DeleteShardGroup(database, policy, id)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // PrecreateShardGroups creates shard groups whose endtime is before the 'to' time passed in, but
 // is yet to expire before 'from'. This is to avoid the need for these shards to be created when data
 // for the corresponding time range arrives. Shard creation involves Raft consensus, and precreation
 // avoids taking the hit at write-time.
-// TODO: protobuf
 func (c *Client) PrecreateShardGroups(from, to time.Time) error {
-	return c.Client.PrecreateShardGroups(from, to)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.PrecreateShardGroups(from, to)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // ShardOwner returns the owning shard group info for a specific shard.
@@ -311,33 +280,48 @@ func (c *Client) ShardOwner(shardID uint64) (database, policy string, sgi *meta.
 }
 
 // CreateContinuousQuery foo
-// TODO: protobuf (might)
 func (c *Client) CreateContinuousQuery(database, name, query string) error {
-	return c.Client.CreateContinuousQuery(database, name, query)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.CreateContinuousQuery(database, name, query)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // DropContinuousQuery foo
-// TODO: protobuf (might)
 func (c *Client) DropContinuousQuery(database, name string) error {
-	return c.Client.DropContinuousQuery(database, name)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.DropContinuousQuery(database, name)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // CreateSubscription foo
-// TODO: protobuf (might)
 func (c *Client) CreateSubscription(database, rp, name, mode string, destinations []string) error {
-	return c.Client.CreateSubscription(database, rp, name, mode, destinations)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.CreateSubscription(database, rp, name, mode, destinations)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // DropSubscription foo
-// TODO: protobuf (might)
 func (c *Client) DropSubscription(database, rp, name string) error {
-	return c.Client.DropSubscription(database, rp, name)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.DropSubscription(database, rp, name)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // SetData foo
-// TODO: protobuf (might)
 func (c *Client) SetData(data *meta.Data) error {
-	return c.Client.SetData(data)
+	f := func(c *Client) (interface{}, error) {
+		return nil, c.Client.SetData(data)
+	}
+	_, err := c.ModifyAndSync(f)
+	return err
 }
 
 // Data foo
@@ -358,13 +342,124 @@ func (c *Client) MarshalBinary() ([]byte, error) {
 
 // SetLogOutput sets the writer to which all logs are written. It must not be
 // called after Open is called.
-// TODO: protobuf (might)
 func (c *Client) SetLogOutput(w io.Writer) {
 	c.Client.SetLogOutput(w)
 }
 
 // Load will save the current meta data from disk
-// TODO: protobuf (might)
+// TODO: should it call ModifyAndSync? It only updates the cache
 func (c *Client) Load() error {
 	return c.Client.Load()
+}
+
+// mutex is used to lock write (local and to cluster)
+var mutex = &sync.Mutex{}
+
+// ModifyAndSync foo
+func (c *Client) ModifyAndSync(f func(c *Client) (interface{}, error)) (interface{}, error) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	var val interface{}
+	var err error
+
+	lastVersion := Version
+	lastSnapshot := c.Data()
+	for i := 0; i < 5; i++ {
+		// apply function that modifies current snapshot
+		val, err = f(c)
+		if err != nil {
+			c.SetData(&lastSnapshot)
+			break
+		}
+
+		// send modified snapshot to cflux
+		var response ClusterResponse
+		response, err = c.postToCflux(viper.GetString("CLUSTER"))
+
+		if err != nil {
+			break
+		}
+
+		// if response is conflict, try to reset local data to receied data
+		if response.Status == http.StatusConflict {
+			tempData := meta.Data{}
+			err = (&tempData).UnmarshalBinary(response.Body)
+			if err != nil {
+				break
+			}
+			err = c.SetData(&tempData)
+			if err != nil {
+				break
+			}
+			Version = response.Version
+			lastVersion = response.Version
+			lastSnapshot = tempData
+		} else if response.Status == http.StatusOK {
+			// success!
+			Version = response.Version
+			return val, nil
+		} else {
+			// response status is bad (e.g. 500)
+			err = errors.New(string(response.Body))
+			break
+		}
+	}
+
+	// rollbak on error
+	Version = lastVersion
+	if err2 := c.SetData(&lastSnapshot); err2 != nil {
+		return nil, err2
+	}
+	return nil, err
+}
+
+func (c *Client) postToCflux(cluster string) (ClusterResponse, error) {
+	url := viper.GetString("CFLUX_ENDPOINT") + "/" + url.QueryEscape(cluster) + "/" + Version
+	cdata := c.Data()
+	data, err := (&cdata).MarshalBinary()
+	if err != nil {
+		return ClusterResponse{}, err
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	resp, err := c.expBackoffRequest(cluster, *req)
+	if err != nil {
+		return ClusterResponse{}, err
+	}
+	response, err := c.readResponse(resp)
+	if err != nil {
+		return ClusterResponse{}, err
+	}
+	return response, nil
+}
+
+func (c *Client) expBackoffRequest(cluster string, req http.Request) (*http.Response, error) {
+	client := &http.Client{}
+	var resp *http.Response
+	var err error
+
+	for attempt := 1; attempt < 6; attempt++ {
+		resp, err = client.Do(&req)
+		if err == nil {
+			break
+		}
+		backoff := (math.Pow(2, float64(attempt)) - 1) / 2
+		time.Sleep(time.Duration(backoff) * time.Second)
+	}
+	return resp, err
+}
+
+func (c *Client) readResponse(resp *http.Response) (ClusterResponse, error) {
+	status := resp.StatusCode
+	version := resp.Header.Get("version")
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ClusterResponse{}, err
+	}
+	response := ClusterResponse{
+		Body:    respBody,
+		Version: version,
+		Status:  status,
+	}
+	return response, nil
 }
